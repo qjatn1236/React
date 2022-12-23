3adbdb49307636ec85a555b7a0ea08724b591f97
qjatn1236 <qjatn1236@gmail.com>
2022-12-23 15:36:54 +0900

    depth-study

--- depth-app/src/App.js
+++ depth-app/src/App.js
@@ -1,34 +1,33 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import Tab from './components/Tab'
+import './App.css';
+import React from 'react';
+import Tab from './components/Tab';
 
-import { TAB_LIST } from './mock'
+import { TAB_LIST } from './mock';
 
 function App() {
-  // api call
-  const [tablist, setTabList] = useState([])
+	// // api call
+	// const [tablist, setTabList] = useState([]);
 
-  useEffect(() => {
-    // fetch
-    setTabList([]) // response.dat
-  }, [])
+	// useEffect(() => {
+	// 	// fetch
+	// 	setTabList([]); // response.dat
+	// }, []);
 
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <div className='tab-container'>
-          {TAB_LIST.map((item) => (
-            <Tab
-              key={item.title}
-              title={item.title}
-              children={item.children}
-              img={item.img}
-            />
-          ))}
-        </div>
-      </header>
-    </div>
-  )
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<div className='tab-container'>
+					{TAB_LIST.map((item) => (
+						<Tab
+							key={item.title}
+							title={item.title}
+							children={item.children}
+							img={item.img}
+						/>
+					))}
+				</div>
+			</header>
+		</div>
+	);
 }
-
-export default App
+export default App;
--- depth-app/src/components/Tab.js
+++ depth-app/src/components/Tab.js
@@ -1,32 +1,29 @@
-import '../App.css'
-
-import { useState } from 'react'
+import React, { useState } from 'react';
 
 function Tab({ title, children, img }) {
-  const [isHover, setIsHover] = useState(false)
+	const [isHover, setIsHover] = useState(false);
 
-  return (
-    <div
-      className='container'
-      onMouseOver={() => setIsHover(true)}
-      onMouseLeave={() => setIsHover(false)}
-    >
-      <div>{children ? <span>{title}</span> : <a href='/'>{title}</a>}</div>
-      {isHover &&
-        children &&
-        children.map((child) => (
-          <div className='child-container'>
-            <Tab
-              key={child.title}
-              title={child.title}
-              children={child.children}
-              img={child.img}
-            />
-            {/* {img && <img src={img} />} */}
-          </div>
-        ))}
-    </div>
-  )
+	return (
+		<div
+			className='container'
+			onMouseOver={() => setIsHover(true)}
+			onMouseLeave={() => setIsHover(false)}
+		>
+			<div>{children ? <span>{title}</span> : <a href='/'>{title}</a>}</div>
+			{isHover &&
+				children &&
+				children.map((child) => (
+					<div className='child-container'>
+						<Tab
+							key={child.title}
+							title={child.title}
+							children={child.children}
+							img={child.img}
+						/>
+						{/* {img && <img src={img} />} */}
+					</div>
+				))}
+		</div>
+	);
 }
-
-export default Tab
+export default Tab;
