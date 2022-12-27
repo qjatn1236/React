551666f8e59696fcd6087536933dd99c7ac0bd5e
qjatn1236 <qjatn1236@gmail.com>
2022-12-27 10:44:34 +0900

    react_basic

---
+++ react_basic/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+// import { useState } from 'react';
+
+function App() {
+	return (
+		<div className='App'>
+			<input type='text' />
+			<button>Upload</button>
+		</div>
+	);
+}
+
+export default App;
