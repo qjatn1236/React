b7fc5221e56d2c9d7a1afc143b0cd50ea63dfe10
qjatn1236 <qjatn1236@gmail.com>
2023-01-05 16:01:23 +0900

    별코딩 useContext

---
+++ react-star/src/components/Page.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+
+export default function Page() {
+	return (
+		<div>
+			<Header />
+			<Content />
+			<Footer />
+		</div>
+	);
+}
