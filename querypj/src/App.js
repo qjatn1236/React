777e6440432f8c95ea253b8e3522f17a0ef81fa9
beonsoo Kim <qjatn1236@gmail.com>
2024-02-15 18:05:16 +0900

    react query

--- querypj/src/App.js
+++ querypj/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import MainProducts from "./components/MainProducts";
-
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
@@ -10,6 +10,7 @@ export default function App() {
     <>
       <QueryClientProvider client={queryClient}>
         <MainProducts />
+        <ReactQueryDevtools initialIsOpen={true} />
       </QueryClientProvider>
       ;
     </>
--- querypj/src/components/Products.jsx
+++ querypj/src/components/Products.jsx
@@ -15,7 +15,23 @@ export default function Products() {
       const response = await fetch(`data/${checked ? "sale_" : ""}products.json`);
       return response.json();
     },
+    staleTime: 1000 * 60 * 5,
   });
+
+  // const {
+  //   isLoading,
+  //   error,
+  //   data: products,
+  // } = useQuery({
+  //   queryKey: ["products", checked],
+  //   queryFn: async () => {
+  //     console.log("fetching...");
+  //     return fetch(`data/${checked ? "sale_" : ""}products.json`).then((res) =>
+  //       res.json()
+  //     );
+  //   },
+  //   staleTime: 5000,
+  // });
 
   const handleChange = () => setChecked((prev) => !prev);
 
