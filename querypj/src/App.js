f63ff3ac2a28f701d4a68e10a63b92afbc088f89
beonsoo Kim <qjatn1236@gmail.com>
2024-02-15 09:52:13 +0900

    react-query 파일

---
+++ querypj/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import MainProducts from "./components/MainProducts";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+
+export default function App() {
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <MainProducts />
+      </QueryClientProvider>
+      ;
+    </>
+  );
+}
