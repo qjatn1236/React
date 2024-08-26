5b2ef04c3c1fd1ff8fbf524ee7e3e29a911a3de2
김범수 <qjatn1236@naver.com>
2024-08-26 22:29:05 +0900

    todo

---
+++ todo/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState, useRef } from "react";
+import "./App.css";
+import Editor from "./components/Editor";
+import Header from "./components/Header";
+import List from "./components/List";
+const mockData = [
+  {
+    id: 0,
+    isDone: true,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래연습",
+    date: new Date().getTime(),
+  },
+];
+
+function App() {
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+
+    setTodos([newTodo, ...todos]);
+  };
+
+  const onUpdate = (targetId) => {
+    setTodos(todos.map((todo) => (todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo)));
+  };
+
+  const onDelete = (targetId) => {
+    setTodos(todos.filter((todo) => todo.id !== targetId));
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <Editor onCreate={onCreate} />
+      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
+    </div>
+  );
+}
+
+export default App;
---
+++ todo/src/components/Editor.jsx
@@ -0,0 +1,34 @@
+import { useState, useRef } from "react";
+import "./Editor.css";
+
+export default function Editor({ onCreate }) {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onSubmit = () => {
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+
+    onCreate(content);
+    setContent("");
+  };
+
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  return (
+    <div className="Editor">
+      <input onChange={onChangeContent} onKeyDown={onKeyDown} ref={contentRef} type="text" placeholder="새로운 Todo ..." value={content} />
+      <button onClick={onSubmit}>추가</button>
+    </div>
+  );
+}
